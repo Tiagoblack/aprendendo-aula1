3c6d5a8fe54afa3368c08e8070ab04eb78b3100d
tiago dos santos <xlzthiago@gmail.com>
2021-10-28 10:30:45 -0300

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, SafeAreaView, TextInput, Button } from 'react-native';
+import AsycnStorage from '@react-native-async-storage/async-storage'
 
 export default function App() {
+
+    const [nome, setNome] = useState('');
+    const [nomeNovo, setnomeNovo ] = useState('')
+    
+
+    const handleSave = async() =>{
+      await AsycnStorage.setItem('@nome', nomeNovo);
+      setNome(nomeNovo);
+      setnomeNovo('')
+    }
+
+      const handleSaveGet = async() =>{
+        const n = await AsycnStorage.getItem('@nome');
+        setNome(n)
+
+      }
+
+      useEffect(()=>{
+        handleSaveGet()
+      }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+        <TextInput placeholder="Digite algo novo" 
+        onChangeText={(n)=>setnomeNovo(n)}
+        value={nomeNovo}
+        style={{height:40, width:'90%', borderColor:'#000', borderWidth:1, marginTop:20, marginBottom:20}}/>
+        <Button
+        onPress={handleSave}
+         title="Salvar"/>
+          <Text>{nome}</Text>
+      <StatusBar style="auto" hidden />
+    </SafeAreaView>
   );
 }
 
@@ -16,6 +45,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
